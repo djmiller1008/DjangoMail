1332281ea5435d6031e2bfc39e6284f2575d6a04
David <djmiller1008@yahoo.com>
2022-09-29 00:31:59 +0800

    Add single email load function, add mark as read function, add archive function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+
   document.querySelector('#compose-form').addEventListener('submit', event => send_email(event));
 
 
@@ -19,7 +20,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
-  
+  document.querySelector('#single-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -33,7 +34,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
+  document.querySelector('#single-email-view').style.display = 'none';
     
 
   // Show the mailbox name
@@ -102,6 +103,7 @@ function get_sent_emails() {
       result.forEach(email => {
         const section = document.createElement('section');
         section.className = 'email-display-section'
+        
         const p1 = document.createElement('span');
         const p2 = document.createElement('span');
         const p3 = document.createElement('span');
@@ -116,6 +118,8 @@ function get_sent_emails() {
         section.appendChild(p1);
         section.appendChild(p2);
         section.appendChild(p3);
+
+        section.addEventListener('click', () => load_email(email.id, 'sent'))
   
         emailDiv.appendChild(section);
       })
@@ -124,13 +128,19 @@ function get_sent_emails() {
 
 function get_inbox_emails() {
   const emailDiv = document.querySelector('#emails-view');
-
+ 
   fetch('/emails/inbox')
     .then(response => response.json())
     .then(result => {
       result.forEach(email => {
         const section = document.createElement('section');
-        section.className = 'email-display-section'
+
+        if (email.read) {
+          section.className = 'email-display-section read';
+        } else {
+          section.className = 'email-display-section';
+        }
+        
         const p1 = document.createElement('span');
         const p2 = document.createElement('span');
         const p3 = document.createElement('span');
@@ -145,8 +155,126 @@ function get_inbox_emails() {
         section.appendChild(p1);
         section.appendChild(p2);
         section.appendChild(p3);
+
+        section.addEventListener('click', () => load_email(email.id, 'inbox'))
   
         emailDiv.appendChild(section);
       })
     })
+}
+
+function markAsRead(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  })
+}
+
+function archiveEmail(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: true
+    })
+  })
+}
+
+function load_email(id, fromMailbox) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'block';
+
+  fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(result => {
+
+      
+      const emailDiv = document.querySelector('#single-email-view');
+      emailDiv.replaceChildren();
+      const emailInfoDiv = document.createElement('div');
+      emailInfoDiv.className = 'single-email-div';
+
+      const fromSection = document.createElement('section');
+      fromSection.className = 'single-email-info';
+
+      const from = document.createElement('span');
+      from.innerHTML = result.sender;
+      const fromLabel = document.createElement('strong');
+      fromLabel.innerHTML = 'From: ';
+
+      fromSection.appendChild(fromLabel);
+      fromSection.appendChild(from);
+
+      const toSection = document.createElement('section');
+      toSection.className = 'single-email-info';
+
+      const to = document.createElement('span');
+      to.innerHTML = result.recipients;
+      const toLabel = document.createElement('strong');
+      toLabel.innerHTML = 'To: ';
+
+      toSection.appendChild(toLabel);
+      toSection.appendChild(to);
+
+      const subjectSection = document.createElement('section');
+      subjectSection.className = 'single-email-info';
+
+      const subject = document.createElement('span');
+      subject.innerHTML = result.subject;
+      const subjectLabel = document.createElement('strong');
+      subjectLabel.innerHTML = 'Subject: '
+
+      subjectSection.appendChild(subjectLabel);
+      subjectSection.appendChild(subject);
+
+      const timestampSection = document.createElement('section');
+      timestampSection.className = 'single-email-info';
+
+      const timestamp = document.createElement('span');
+      timestamp.innerHTML = result.timestamp;
+      const timestampLabel = document.createElement('strong');
+      timestampLabel.innerHTML = 'Timestamp: '
+
+      timestampSection.appendChild(timestampLabel);
+      timestampSection.appendChild(timestamp);
+
+      emailInfoDiv.appendChild(fromSection);
+      emailInfoDiv.appendChild(toSection);
+      emailInfoDiv.appendChild(subjectSection);
+      emailInfoDiv.appendChild(timestampSection);
+
+      emailDiv.appendChild(emailInfoDiv);
+
+      const replyButton = document.createElement('button');
+      replyButton.innerHTML = 'Reply';
+      replyButton.className = 'btn btn-sm btn-outline-primary'
+      emailDiv.appendChild(replyButton);
+
+      if (fromMailbox === 'inbox') {
+      
+        const archiveButton = document.createElement('button');
+
+        if (result.archived) {
+          archiveButton.innerHTML = 'Unarchive';
+        } else {
+          archiveButton.innerHTML = 'Archive';
+        }
+        
+        archiveButton.className = 'btn btn-sm btn-outline-primary archive';
+        archiveButton.addEventListener('click', () => archiveEmail(id));
+        emailDiv.appendChild(archiveButton);
+      }
+     
+      const hr = document.createElement('hr');
+      emailDiv.appendChild(hr);
+
+      const bodyP = document.createElement('p');
+      bodyP.innerHTML = result.body;
+
+      emailDiv.appendChild(bodyP);
+
+    })
+    markAsRead(id);
 }
